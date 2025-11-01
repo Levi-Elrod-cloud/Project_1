7fa806dbef67c49ce955a9665610c99836eedf26
Levi-Elrod-cloud <elrodlevi3@gmail.com>
2025-11-01 11:38:42 -0400

    Work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
-
+    public float turnSpeed;
     public int myNumber = 0;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,9 +16,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //Debug.Log("My number is " + myNumber);
         //Adds 1 every frame to myNumber
         //myNumber += 1;
-        transform.Translate(0 , 0, 2 * Time.deltaTime * speed);
+        transform.Translate(0 , 0, 2 * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
